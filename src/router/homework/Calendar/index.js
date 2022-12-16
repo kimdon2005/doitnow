25d9ae0b53278b653b7c07aff3a0eb9fe421abac
현상 <85813814+hyux25@users.noreply.github.com>
2022-12-16 20:34:58 +0900

    Update index.js

--- src/router/homework/Calendar/index.js
+++ src/router/homework/Calendar/index.js
@@ -6,6 +6,52 @@ import Navigation from "components/Navigation/Index";
 const Calender = () => {
   const [value, onChange] = useState(new Date());
 
+  const toDoList = [
+    {
+      id: "283o4234823798",
+      title: "영어 프린트 수행평가1",
+      from: "2022년 9월 10일",
+      to: "27일"
+    },
+    {
+      id: "283o423432423423423823798",
+      title: "영어 프린트 수행평가2",
+      from: "2022년 9월 29일",
+      to: "30일"
+    }
+  ];
+
+  const ViewList = ({ info }) => {
+    return (
+      <div className="rectangle-11">
+        <div className="left">
+          <div></div>
+        </div>
+        <div className="center">
+          <div>
+            <p className="title">{info.title}</p>
+            <p className="date">
+              {info.from} ~ {info.to}
+            </p>
+          </div>
+        </div>
+        <div className="right">
+          <p>></p>
+        </div>
+      </div>
+    );
+  };
+
+  const PrintList = () => {
+    return (
+      <div>
+        {toDoList.map((info) => (
+          <ViewList info={info} />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
       <Calendar />
@@ -15,20 +61,7 @@ const Calender = () => {
           <p className="today-date">27일 수요일</p>
         </div>
         <div className="list">
-          <div className="rectangle-11">
-            <div className="left">
-              <div></div>
-            </div>
-            <div className="center">
-              <div>
-                <p className="title">영어 프린트 수행평가</p>
-                <p className="date">2022년 6월 24일 ~ 27일</p>
-              </div>
-            </div>
-            <div className="right">
-              <p>></p>
-            </div>
-          </div>
+          <PrintList />
         </div>
       </div>
       <Navigation />
